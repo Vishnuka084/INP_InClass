8a7bd792d31094665f9a2e131e7b89cffd1796c1
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 13:43:32 +0530

    Updated content

--- module_01/src/lk/ijse/ServerSide.java
+++ module_01/src/lk/ijse/ServerSide.java
@@ -5,8 +5,8 @@ import java.net.ServerSocket;
 
 public class ServerSide {
     public static void main(String[] args) throws IOException {
-        int port = 8000;
-        ServerSocket serverSocket = new ServerSocket(port);
-        System.out.println("server is up & running on port :"+port);
+        final int PORT = 8000;
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("server is up & running on port :"+PORT);
     }
 }
