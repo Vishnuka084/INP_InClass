6e2d898f4da58180ff4fd72c1c2eac585ac64b71
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 21:38:27 +0530

    Updated

--- module-04/src/Demo.java
+++ module-04/src/Demo.java
@@ -1,19 +1,33 @@
-import java.io.BufferedInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) throws IOException {
        /* System.out.print("Input a char ;" );
         int code = System.in.read();
         System.out.println("Code :" +(char)code);
-*/
+        */
 
-        System.out.println(System.in);
+        /*System.out.println(System.in);
         BufferedInputStream bis = (BufferedInputStream) System.in;
         Scanner input = new Scanner(bis);
         System.out.println("Input a Number :");
         int x = input.nextInt();
-        System.out.println(x);
+        System.out.println(x);*/
+
+
+        FileInputStream fis = new FileInputStream("src/Customers.txt");
+        int code = fis.read();
+        System.out.println("Code "+(char)code);
+
+        code = fis.read();
+        System.out.println("Code "+(char)code);
+
+        code = fis.read();
+        System.out.println("Code "+(char)code);
+
+
+
+
     }
 }
