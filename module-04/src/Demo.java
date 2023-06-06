be46efeec36c3ae0a38cea096d94cea77c36c92d
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 20:23:12 +0530

    Updated

---
+++ module-04/src/Demo.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Input a char ;" );
+        int code = System.in.read();
+
+    }
+}
