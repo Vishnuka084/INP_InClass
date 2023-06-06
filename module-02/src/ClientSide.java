5e34b716228fb228cc4160663da133bda63888de
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 15:16:41 +0530

    Updated content

--- module-02/src/ClientSide.java
+++ module-02/src/ClientSide.java
@@ -1,9 +1,17 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ClientSide {
     public static void main(String[] args) throws IOException {
         final int PORT = 8000;
         Socket socket = new Socket("localhost",PORT);
+
+        PrintWriter printWriter = new PrintWriter(
+                socket . getOutputStream()
+        );
+        printWriter.println("Hello server");
+        printWriter.flush();
+
     }
 }
