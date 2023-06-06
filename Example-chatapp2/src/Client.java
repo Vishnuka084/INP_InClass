eb679727677a7f8f46a27a032c97ab0e7cd5f12b
“Vishnu <vishnukayahan84@gmail.com>
2023-06-07 00:06:40 +0530

    Updated content

--- Example-chatapp2/src/Client.java
+++ Example-chatapp2/src/Client.java
@@ -1,2 +1,12 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1",3000);
+        PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+        printWriter.println("Hello server");
+        printWriter.flush();
+    }
 }
--- Example-chatapp2/src/Server.java
+++ Example-chatapp2/src/Server.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
@@ -9,5 +10,8 @@ public class Server {
         System.out.println("Server started");
         Socket acceptedConnection = serverSocket.accept();
         InputStreamReader inputStreamReader =  new InputStreamReader(acceptedConnection.getInputStream());
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String str = bufferedReader.readLine();
+        System.out.println("Client says ,"+str);
     }
 }
