7336d6dbe564d1ec77e6b8daa27a9e5a803e832a
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 13:45:40 +0530

    Updated

--- module_01/src/lk/ijse/ServerSide.java
+++ module_01/src/lk/ijse/ServerSide.java
@@ -2,11 +2,13 @@ package lk.ijse;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerSide {
     public static void main(String[] args) throws IOException {
         final int PORT = 8000;
         ServerSocket serverSocket = new ServerSocket(PORT);
         System.out.println("server is up & running on port :"+PORT);
+        Socket socket = serverSocket.accept();
     }
 }
