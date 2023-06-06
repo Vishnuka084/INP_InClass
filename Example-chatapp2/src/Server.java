edaedbe5b120af4cdc38235f774d65c0b8949f42
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 23:56:12 +0530

    Updated

---
+++ Example-chatapp2/src/Server.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3000);
+        System.out.println("Server started");
+        Socket acceptedConnection = serverSocket.accept();
+        InputStreamReader inputStreamReader =  new InputStreamReader(acceptedConnection.getInputStream());
+    }
+}
