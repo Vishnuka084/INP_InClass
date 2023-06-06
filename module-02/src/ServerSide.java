a518b4cbf508d15d155d7956c007df74674f6cb9
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 14:48:44 +0530

    Step 02

---
+++ module-02/src/ServerSide.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class ServerSide {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8000; //--> int->Integer , double->Double
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        System.out.println("server is up & Running on port :"+PORT);
+    }
+}
