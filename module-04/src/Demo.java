8d94ccde62f2a2a29be90a19943bfbd50dcbe736
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 20:46:47 +0530

    BufferInputStream introduction

--- module-04/src/Demo.java
+++ module-04/src/Demo.java
@@ -1,10 +1,19 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Demo {
     public static void main(String[] args) throws IOException {
-        System.out.print("Input a char ;" );
+       /* System.out.print("Input a char ;" );
         int code = System.in.read();
         System.out.println("Code :" +(char)code);
+*/
 
+        System.out.println(System.in);
+        BufferedInputStream bis = (BufferedInputStream) System.in;
+        Scanner input = new Scanner(bis);
+        System.out.println("Input a Number :");
+        int x = input.nextInt();
+        System.out.println(x);
     }
 }
