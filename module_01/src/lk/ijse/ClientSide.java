658d71a7006469c5c48e8279c690afbe48a3a124
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 14:41:04 +0530

    Updated content

--- module_01/src/lk/ijse/ClientSide.java
+++ module_01/src/lk/ijse/ClientSide.java
@@ -7,5 +7,7 @@ public class ClientSide {
     public static void main(String[] args) throws IOException {
         final int PORT = 8000;
         Socket socket = new Socket("localhost",PORT);
+
+        //localhost , 127.0.0.1 ,[ipv4]
     }
 }
