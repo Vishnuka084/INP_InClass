47a105e4dc3c8513fd806ea6130121d9c7396f2e
“Vishnu <vishnukayahan84@gmail.com>
2023-06-06 14:58:31 +0530

    Updated content

---
+++ module-02/src/ClientSide.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientSide {
+    public static void main(String[] args) throws IOException {
+        final int PORT = 8000;
+        Socket socket = new Socket("localhost",PORT);
+    }
+}
--- module-02/src/ServerSide.java
+++ module-02/src/ServerSide.java
@@ -1,10 +1,13 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerSide {
     public static void main(String[] args) throws IOException {
         final int PORT = 8000; //--> int->Integer , double->Double
         ServerSocket serverSocket = new ServerSocket(PORT);
         System.out.println("server is up & Running on port :"+PORT);
+        Socket socket = serverSocket.accept();
+
     }
 }
